d164e75dd45db8418597d0847e7d211fa2f5f710
mitikon <kon1_257432@icloud.com>
2026-09-04 20:45:59 +0900

    Extend daily data collection: src/leading_signal_lambda/__init__.py

--- src/leading_signal_lambda/__init__.py
+++ src/leading_signal_lambda/__init__.py
@@ -3,6 +3,7 @@
 from .model import LeadingLambdaClassifier, Prediction
 from .signals import build_leading_features, build_training_set
 from .validation import WalkForwardResult, walk_forward_validate
+from .collector import DailyMarketCollector, MarketDataset, DEFAULT_UNIVERSE
 
 __all__ = [
     "LeadingLambdaClassifier",
@@ -11,5 +12,7 @@ __all__ = [
     "build_leading_features",
     "build_training_set",
     "walk_forward_validate",
+    "DailyMarketCollector",
+    "MarketDataset",
+    "DEFAULT_UNIVERSE",
 ]
-
