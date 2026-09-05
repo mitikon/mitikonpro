f2c2bf8edd874cc0001370aa09ac197bda53acb5
mitikon <kon1_257432@icloud.com>
2026-09-05 22:38:30 +0900

    Export paper PCA SUB API

--- src/leading_signal_lambda/__init__.py
+++ src/leading_signal_lambda/__init__.py
@@ -4,6 +4,8 @@ from .model import LeadingLambdaClassifier, Prediction
 from .signals import build_leading_features, build_training_set
 from .validation import WalkForwardResult, walk_forward_validate
 from .collector import DailyMarketCollector, MarketDataset, DEFAULT_UNIVERSE
+from .paper_backtest import PaperBacktestResult, run_paper_backtest
+from .paper_pca_sub import PaperPcaSubModel, PaperSignal
 
 __all__ = [
     "LeadingLambdaClassifier",
@@ -15,4 +17,8 @@ __all__ = [
     "DailyMarketCollector",
     "MarketDataset",
     "DEFAULT_UNIVERSE",
+    "PaperPcaSubModel",
+    "PaperSignal",
+    "PaperBacktestResult",
+    "run_paper_backtest",
 ]
