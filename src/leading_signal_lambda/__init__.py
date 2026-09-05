7617c5b79ab896a44381e43b3a333d57586ebb82
mitikon <kon1_257432@icloud.com>
2026-09-06 06:08:34 +0900

    Export US-market leading signal API

--- src/leading_signal_lambda/__init__.py
+++ src/leading_signal_lambda/__init__.py
@@ -6,6 +6,8 @@ from .validation import WalkForwardResult, walk_forward_validate
 from .collector import DailyMarketCollector, MarketDataset, DEFAULT_UNIVERSE
 from .paper_backtest import PaperBacktestResult, run_paper_backtest
 from .paper_pca_sub import PaperPcaSubModel, PaperSignal
+from .us_market_pca_sub import USMarketPcaSubModel, USMarketSignal, prepare_us_market_returns
+from .us_market_validation import USMarketBacktestResult, run_us_market_backtest
 
 __all__ = [
     "LeadingLambdaClassifier",
@@ -21,4 +23,9 @@ __all__ = [
     "PaperSignal",
     "PaperBacktestResult",
     "run_paper_backtest",
+    "USMarketPcaSubModel",
+    "USMarketSignal",
+    "prepare_us_market_returns",
+    "USMarketBacktestResult",
+    "run_us_market_backtest",
 ]
